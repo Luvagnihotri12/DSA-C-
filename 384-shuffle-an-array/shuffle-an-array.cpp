d556c8b892dbfcf0e40ff79cd64e4b17a1fef9fa
Luv Agnihotri <luvagnihotri07@gmail.com>
2025-09-22 23:57:30 +0530

    Time: 8 ms (73.15%) | Memory: 63.6 MB (43.01%) - LeetSync

---
+++ 384-shuffle-an-array/shuffle-an-array.cpp
@@ -0,0 +1,29 @@
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+class Solution {
+private:
+    vector<int> original;
+
+public:
+    Solution(vector<int>& nums) {
+        original = nums;
+        srand(time(nullptr)); // Seed for randomness
+    }
+
+    vector<int> reset() {
+        return original;
+    }
+
+    vector<int> shuffle() {
+        vector<int> shuffled = original;
+        int n = shuffled.size();
+        for (int i = n - 1; i > 0; --i) {
+            int j = rand() % (i + 1); // Random index from 0 to i
+            swap(shuffled[i], shuffled[j]);
+        }
+        return shuffled;
+    }
+};
